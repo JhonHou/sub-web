587ee5bbcbdf785398c03f33f1b9c6897c64c04d
JhonHou <97167982+JhonHou@users.noreply.github.com>
2022-02-11 20:55:52 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "https://subweb-heroku.yunbai.workers.dev/sub?" }],
+        backendOptions: [{ value: "https://winter-cell-88cd.hzm040918.workers.dev/sub?" }],
         remoteConfig: [
         {
             label: "ACL4SSR",
